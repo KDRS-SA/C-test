bbc7c39868b444bd365e85f00398e49eb3b80f3f
Torbjorn Aasen <taa.database@gmail.com>
2014-08-22 19:21:47 +0200

    Randomized Upper & Lower Case

--- Diktifier/Diktifier/Diktifier/Program.cs
+++ Diktifier/Diktifier/Diktifier/Program.cs
@@ -13,6 +13,8 @@ namespace Hello_World
             bool more = true;
             Console.WriteLine("Oppgi en tekststreng som kan 'dikt-i-fiseres' etter Jan Erik Wold-metoden:"+Environment.NewLine+"(For eksempel 'kulturuke')");
             string streng = Console.ReadLine().ToLower();
+            int nUpper = 0;
+            int nLower = 0;
             while (more)
             {
                 for (int a = 0; a < 10; a++)
@@ -41,8 +43,48 @@ namespace Hello_World
             a[pos1] = a[pos2];
             a[pos2] = temp;
             utStreng = new String(a);
+            utStreng = RandomLowerUpperString(utStreng);
             Console.WriteLine(utStreng);
             return utStreng;
         }
+
+        public static string RandomLowerUpperString(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.Empty;
+            // convert to char array of the string
+            char[] letters = source.ToCharArray();
+
+            Random rand = new Random();
+            // Randomize String Upper and Lower Case
+            for (int i = 0; i < letters.Length; i++)
+            {
+                Double tmpDouble = rand.NextDouble();
+                if (tmpDouble > 0.5)
+                {
+                    letters[i] = char.ToUpper(letters[i]);
+                }
+                else
+                {
+                    letters[i] = char.ToLower(letters[i]);
+                }
+            }
+
+            // return the array made of the new char array
+            return new string(letters);
+        }
+
+        public static string ToUpperFirstLetter(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.Empty;
+            // convert to char array of the string
+            char[] letters = source.ToCharArray();
+            // upper case the first char
+            letters[0] = char.ToUpper(letters[0]);
+            // return the array made of the new char array
+            return new string(letters);
+        }
+
     }
 }
